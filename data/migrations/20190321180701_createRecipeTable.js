ee969843e7c4d8dc6b9ba0b170749486009ffaf8
Ashley Owens <ash4741@gmail.com>
2019-03-21 19:44:16 -0400

    Set up Migrations

--- data/migrations/20190321180701_createRecipeTable.js
+++ data/migrations/20190321180701_createRecipeTable.js
@@ -3,7 +3,6 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("recipe", table => {
     table.increments();
     table.string("recipe_name")
-    table.float("quantity");
 
     table.integer("dish_id")
       .unsigned()
--- data/migrations/20190321180720_createIngredientTable.js
+++ data/migrations/20190321180720_createIngredientTable.js
@@ -3,6 +3,8 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("ingredient", table => {
     table.increments();
     table.string("ingredient_name");
+    table.float("quantity");
+    
   });
 };
 
